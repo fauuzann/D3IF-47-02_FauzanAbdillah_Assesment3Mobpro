e6c356c34647dc94da66c3ae60cc756c73544ee1
Fauzan <fauzannabdillah21@gmail.com>
2025-06-17 03:24:05 +0700

    Step 5: Tambahkan UI List Item pertama

---
+++ app/src/main/java/com/fauzana0133/assesmentmobpro3/screen/ItemListScreen.kt
@@ -0,0 +1,37 @@
+package com.fauzana0133.assesmentmobpro3.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.fauzana0133.assesmentmobpro3.viewmodel.ItemViewModel
+
+@Composable
+fun ItemListScreen(viewModel: ItemViewModel) {
+    val items = viewModel.itemList
+    val loading = viewModel.isLoading
+
+    if (loading) {
+        CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+    } else {
+        LazyColumn {
+            items(items) { item ->
+                Card(modifier = Modifier.padding(8.dp)) {
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        AsyncImage(
+                            model = item.imageUrl,
+                            contentDescription = null,
+                            modifier = Modifier.size(200.dp)
+                        )
+                        Text(item.name, style = MaterialTheme.typography.titleLarge)
+                        Text(item.description)
+                    }
+                }
+            }
+        }
+    }
+}
